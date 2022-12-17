73525a86515d324321196487897e3598f19ab4ed
taekbeom <oumamarrysaihara@gmail.com>
2022-12-18 00:26:47 +0300

    fixed post_author

--- post_procedure.sql
+++ post_procedure.sql
@@ -15,13 +15,23 @@ BEGIN
                          WHERE account_login = set_author_login) > 0
     AND (new_post_text IS NOT NULL)
     AND ((SELECT COUNT(*) FROM post
-                          WHERE post_id = new_reply_post_id) > 0
+                          WHERE post_id = new_reply_post_id
+                          AND forum_id = set_forum_id) > 0
              AND new_category_id IS NULL
              OR
          (new_reply_post_id IS NULL AND
           (new_category_id IS NULL OR
            (SELECT COUNT(*) FROM post_category
-                          WHERE category_id = new_category_id) > 0))
+                          WHERE category_id = new_category_id) > 0)
+             AND (SELECT COUNT(*) FROM profile
+                JOIN member_profile ON
+                    profile.profile_id = member_profile.profile_id
+                JOIN member ON
+                    member_profile.member_id = member.member_id
+                JOIN forum ON
+                    member.group_id = forum.group_id
+                WHERE profile.account_login = set_author_login
+                AND forum.forum_id = set_forum_id) > 0)
          )
         THEN
         INSERT INTO post
