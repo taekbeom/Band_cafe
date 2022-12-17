591e35632e33b891f05b17f078042f3c6012a7c7
taekbeom <oumamarrysaihara@gmail.com>
2022-12-17 21:59:32 +0300

    oops

--- dont_talk.sql
+++ dont_talk.sql
@@ -1,7 +1,7 @@
-INSERT INTO account
-VALUES ('olleg', crypt('HF*w3hI9ZWL7JBoRy243&#ohV5YI9Zp', gen_salt('bf', 8)), 0);
-CREATE USER olleg WITH PASSWORD 'HF*w3hI9ZWL7JBoRy243&#ohV5YI9Zp';
+CALL add_user('olleg', 'HF*w3hI9ZWL7JBoRy243&#ohV5YI9Zp');
+CALL update_user('olleg', new_role_id := 0);
 GRANT admin_role TO olleg;
 ALTER ROLE admin_role CREATEROLE;
 ALTER USER olleg CREATEROLE;
 GRANT pg_write_server_files TO olleg;
+SELECT * FROM account;
